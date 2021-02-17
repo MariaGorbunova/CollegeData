ba7fad0a8dc3c43a959cd445e226e6c8c6b60a13
Maria Gorbunova <maria.gorbunova@yahoo.com>
2021-02-16 19:10:05 -0800

    moved some vars in constructor

--- lab3front.py
+++ lab3front.py
@@ -1,7 +1,7 @@
 # Maria Gorbunova
 # Lab3frontend is a GUI that opens windows with the colleges of the sector that user requested
 
-# Reviewed my code with Ben Lublin
+# Reviewed my code with Ben Lublin. Reviewed his code
 
 import tkinter as tk
 import webbrowser
@@ -14,6 +14,20 @@ class MainWin(tk.Tk):
         super().__init__()
         self.conn = sqlite3.connect('lab3back.db')
         self.cur = self.conn.cursor()
+
+        # fetch the db header and save it in row_names
+        self.cur.execute("PRAGMA TABLE_INFO( CollegesDB )")
+        val = self.cur.fetchall()
+        self.row_names = [t[1] for t in val]
+        # row_names: ['name', 'sector_id', 'earlyPay', 'midPay', 'stem', 'url']
+        self.cur.execute("PRAGMA TABLE_INFO( SectorsDB )")
+        val = self.cur.fetchall()
+        self.sectors_ = [t[1] for t in val]
+        self.cur.execute('SELECT DISTINCT {} FROM SectorsDB'.format(self.sectors_[1]))
+        val = self.cur.fetchall()
+        self.types_sectors = [t[0] for t in val[::-1]]
+        # ['Public', 'Private not-for-profit']
+
         self.geometry("350x100")
         self.title("Colleges")
         self.label = tk.Label(self, text="Two-Year College Ranking", fg="blue").grid()
@@ -36,7 +50,7 @@ class MainWin(tk.Tk):
     def new_window(self, idx):
         """this method is called by event when user pushes a button.
         It opens the sector choice window and then the display window"""
-        newWin = ChoiceWin(self)
+        newWin = ChoiceWin(self, self.types_sectors.copy())
         self.wait_window(newWin)
         # gets users choice in a idx format so can use it later
         choice = newWin.get_choice()
@@ -45,38 +59,27 @@ class MainWin(tk.Tk):
 
     def valid_choice(self, idx, choice):
         """if the user choice is valid start the work with databases"""
-        # fetch the db header and save it in row_names
-        self.cur.execute("PRAGMA TABLE_INFO( CollegesDB )")
-        val = self.cur.fetchall()
-        row_names = [t[1] for t in val]
-        # row_names: ['name', 'sector_id', 'earlyPay', 'midPay', 'stem', 'url']
-
-        self.cur.execute("PRAGMA TABLE_INFO( SectorsDB )")
-        # val = self.cur.fetchall()
-        # sectors_ = [t[1] for t in val]
-        # print(sectors_)
-
-        sector = ['Public', 'Private not-for-profit', '%']
-        # decided to not do this cause in any case a different size of a tuple will break it// maybe?
-        # self.cur.execute('SELECT DISTINCT {} FROM CollegesDB'.format(row_names[1]))
-
+        sectors = self.types_sectors.copy()
+        sectors.append('%')
         print_options = [
             '''SELECT  {},{} FROM CollegesDB JOIN SectorsDB ON
-            CollegesDB.{sid} = SectorsDB.{sid} AND
-            SectorsDB.sector LIKE ? ORDER BY CollegesDB.ROWID'''.format(row_names[0], row_names[5], sid=row_names[1]),
-            self.build_q(row_names, 2), self.build_q(row_names, 3), self.build_q(row_names, 4)]
+            CollegesDB.{sid} = SectorsDB.{} AND
+            SectorsDB.{} LIKE ? ORDER BY CollegesDB.ROWID'''.format(self.row_names[0], self.row_names[5],
+                                                                    self.sectors_[0], self.sectors_[1], sid=self.row_names[1]),
+            self.build_q(2), self.build_q(3), self.build_q(4)]
         # execute the sql command with the sector choice
-        self.cur.execute(print_options[idx], (sector[choice],))
+        self.cur.execute(print_options[idx], (sectors[choice],))
         myresult = self.cur.fetchall()
         # if user made a choice open display window
         DisplayWin(self, myresult, self.textlist[idx], idx)
 
-    def build_q(self, row_names, column):
+    def build_q(self, column):
         """ build the query depending on the button option"""
         return '''SELECT {}, {}, {} FROM CollegesDB JOIN SectorsDB ON
-            CollegesDB.{sid} = SectorsDB.{sid} AND
-            SectorsDB.sector LIKE ?  ORDER BY {} DESC'''.format(row_names[0], row_names[column], row_names[5],
-                                                                row_names[column], sid=row_names[1])
+            CollegesDB.{sid} = SectorsDB.{} AND
+            SectorsDB.{} LIKE ?  ORDER BY {} DESC'''.format(self.row_names[0], self.row_names[column],
+                                                            self.row_names[5], self.sectors_[0], self.sectors_[1],
+                                                            self.row_names[column], sid=self.row_names[1])
 
     def exit_fct(self):
         """ closes the window, closes the connection with sql, exits the program"""
@@ -88,7 +91,7 @@ class MainWin(tk.Tk):
 class ChoiceWin(tk.Toplevel):
     """Toplevel window to prompt the user to pick type of college"""
 
-    def __init__(self, master):
+    def __init__(self, master, sectors):
         """constructor creates the window of certain size, has label, radiobuttons and Ok button"""
         super().__init__(master)
         self.geometry("200x125")
@@ -96,8 +99,11 @@ class ChoiceWin(tk.Toplevel):
         self.focus_set()
         self.choice = -1
         self.label = tk.Label(self, text="Choose type of college", fg="black").grid(sticky="W")
+
         # for printing name of the radiobuttons
-        self.rb_variables = ['Public', 'Private not-for-profit', 'Both']
+        self.rb_variables = sectors
+        self.rb_variables.append('Both')
+
         self.controlVar = tk.IntVar()
         for i, item in enumerate(self.rb_variables):
             tk.Radiobutton(self, text=item, variable=self.controlVar, value=i).grid(sticky="W")
